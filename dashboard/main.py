76bdac497c220db70ebe4fb79493f028374cb4e0
Rahim Sharifov <sharifov.rahimcc@gmail.com>
2026-04-04 01:34:49 +0400

    Extract and Transform part of pipeline done

--- dashboard/main.py
+++ dashboard/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from database.models import WeatherRaw, WeatherClean, get_session
+from datetime import datetime
 from routers import metrics
 
 
@@ -9,4 +12,18 @@ app.include_router(metrics.router)
 
 @app.get("/")
 def root():
-    return {"status":"ok "}
+    return {"status":"weather api is running"}
+
+
+@app.get("/weather/clean/latest")
+def get_latest_raw():
+    with get_session as session:
+        record = session.query(WeatherClean).order_by(WeatherClean.\
+                                                      ingested_at.desc()).first()
+
+        if not record:
+            raise HTTPException(status_code=404, detail= "No data found")
+        
+    return record.to_dict() 
+
+
--- flows/weather_api_to_pstgr.py
+++ flows/weather_api_to_pstgr.py
@@ -5,13 +5,13 @@ import sys
 import sqlalchemy
 
 from dotenv import load_dotenv
-from datetime import datetime,timedelta
+from datetime import datetime,timedelta,timezone
 from sqlalchemy import create_engine,text
 from sqlalchemy.orm import Session
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from database.models import WeatherData,get_engine
+from database.models import WeatherRaw,WeatherClean,get_engine
 from prefect import flow, task 
 
 
@@ -49,20 +49,56 @@ def fetch_and_save_raw():
         print("Weather Data extracted successfully")
 
         print(data)
+
+        country = data['sys']['country']
+
         
         engine = get_engine()
-        """
-        with Session() as session:
-             
-             records = [ 
-                  WeatherRaw( 
-                       city = 
-                  )
-             ]
-        """
+        ingested_at = datetime.now(timezone.utc)
+        
+        with Session(engine) as session:
+       
+            records = [  WeatherRaw(city = city, country= country,
+                                     raw_data = data, ingested_at=ingested_at)
+                       ]
+        
+            session.add_all(records)
+            session.commit()
+        
+        print(f"Saved data for {city}")
+
+        return ingested_at
+            
+
+
 @task
-def transform_to_clean():
-    pass
+def transform_to_clean(ingested_at):
+    
+    engine = get_engine() 
+    print('a')
+    with Session(engine) as session:
+        records = session.query(WeatherRaw).\
+        filter(WeatherRaw.ingested_at == ingested_at).first() 
+    
+    records = records.to_dict()
+
+    id = records['id']
+    city = records['city']
+    country = records['country']
+    temperature_c = records['raw_data']['main']['temp']
+    feels_like_c  = records['raw_data']['main']['feels_like']
+    humidity = records['raw_data']['main']['humidity']
+    description = records['raw_data']['weather'][0]['description']
+
+
+    with Session(engine) as session: 
+        
+        new_record = WeatherClean( id = id , city=city, country = country, \
+                                  temperature_c = temperature_c , feels_like_c = feels_like_c,\
+                                  humidity = humidity , description= description
+                                  )
+        session.add(new_record)
+        session.commit()
 
 @task
 def aggregate():
@@ -71,11 +107,13 @@ def aggregate():
 
 @flow
 def weather_pipeline():
-    fetch_and_save_raw()
+    ingested_at = fetch_and_save_raw()
+    print(ingested_at)
+    transform_to_clean(ingested_at)
 
 
 
 if __name__ == "__main__":
-    weather_pipeline()
+    weather_pipeline.serve(name="weather_schedule", interval=timedelta(minutes=1))
 
 
